f2fbc43d789d355a1396c439c85dc05f37008bb8
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-16 00:44:59 +0100

    two middleware

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,27 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import router from "./router";
 import morgan from "morgan";
 
 export const app = express();
 
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
 	req.userName = 'james';
 	next();
-})
+});
 
-app.get("/", (req, res) => {
+const nodeDebugger = (_req: Express.Request, _res: Express.Response, next: NextFunction) => {
+	console.log('in debugger');
+	next();
+}
+
+app.get("/", nodeDebugger, (req, res) => {
 	res.status(200);
 	res.json({ message: "info", userName: req.userName });
 });
 
-app.use("/api", router);
+app.use("/api",morgan('dev'), nodeDebugger,  router);
