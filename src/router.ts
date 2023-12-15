adac1d83d0947efd8d48485a981ffd365ec07891
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-15 22:35:09 +0100

    router api route works

---
+++ src/router.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/product', (req, res) => {
+	res.status(200);
+	res.json({ message: 'getting all products' });
+});
+router.get('/product/:id', () => {});
+router.put('/product/:id', () => {});
+router.post('/product', () => {});
+router.delete('/product/:id', () => {});
+
+router.get('/update', () => {});
+router.get('/update/:id', () => {});
+router.put('/update/:id', () => {});
+router.post('/update', () => {});
+router.delete('/update/:id', () => {});
+
+router.get('/update', () => {});
+router.get('/update/:id', () => {});
+router.put('/update/:id', () => {});
+router.post('/update', () => {});
+router.delete('/update/:id', () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import router from './router';
 
 export const app = express();
 
 app.get('/', (req, res) => {
 	res.status(200);
-	res.json({ message: 'info222' });
+	res.json({ message: 'info' });
 });
 
+app.use('/api', router);
