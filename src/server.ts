153fa8b636a181d854c18fd475b41e1468ebfbff
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-16 10:30:02 +0100

    middleware

--- src/server.ts
+++ src/server.ts
@@ -10,18 +10,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-	req.userName = 'james';
+	req.userName = "james";
 	next();
 });
 
-const nodeDebugger = (_req: Express.Request, _res: Express.Response, next: NextFunction) => {
-	console.log('in debugger');
-	next();
-}
+const nodeDebugger =
+	(kind = "quiet") =>
+	(_req: Express.Request, _res: Express.Response, next: NextFunction) => {
+		console.log(`${kind}: in debugger`);
+		next();
+	};
 
-app.get("/", nodeDebugger, (req, res) => {
+app.get("/", nodeDebugger(), (req, res) => {
 	res.status(200);
 	res.json({ message: "info", userName: req.userName });
 });
 
-app.use("/api",morgan('dev'), nodeDebugger,  router);
+app.use("/api", morgan("dev"), nodeDebugger('verbose'), router);
